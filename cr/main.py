7642655ce1353559154c73f7a8cbb067e23042e3
2easy4wizzi <giladeini@gmail.com>
2018-07-25 22:50:00 +0300

    working

--- cr/main.py
+++ cr/main.py
@@ -4,17 +4,20 @@ import os
 import time
 import shutil
 import pickle
-import logging
 import pandas as pd
 import json
 from collections import Counter
 import numpy as np
 import tensorflow as tf
+from builtins import enumerate
 from sklearn.model_selection import train_test_split
 from text_cnn_rnn import TextCNNRNN
+import logging
 logging.getLogger().setLevel(logging.INFO)
 
 PRO_FLD = ''
+TRA_FLD = 'trained_results_1532544430/'
+TRAIN = False
 
 
 def clean_str(s):  # DATA
@@ -50,7 +53,6 @@ def pad_sentences(sentences, padding_word="<PAD/>", forced_sequence_length=None)
         logging.critical('This is prediction, reading the trained sequence length')
         sequence_length = forced_sequence_length
     logging.critical('The maximum length is {}'.format(sequence_length))
-
     padded_sentences = []
     for i in range(len(sentences)):
         sentence = sentences[i]
@@ -58,6 +60,7 @@ def pad_sentences(sentences, padding_word="<PAD/>", forced_sequence_length=None)
 
         if num_padding < 0:  # Prediction: cut off the sentence if it is longer than the sequence length
             logging.info('This sentence has to be cut off because it is longer than trained sequence length')
+            exit(0)
             padded_sentence = sentence[0:sequence_length]
         else:
             padded_sentence = sentence + [padding_word] * num_padding
@@ -208,8 +211,7 @@ def train_cnn_rnn():  # TRAIN
             sess.run(tf.global_variables_initializer())
 
             # Training starts here
-            train_batches = batch_iter(list(zip(x_train, y_train)), params['batch_size'],
-                                                   params['num_epochs'])
+            train_batches = batch_iter(list(zip(x_train, y_train)), params['batch_size'], params['num_epochs'])
             best_accuracy, best_at_step = 0, 0
 
             # Train the model with x_train and y_train
@@ -263,7 +265,142 @@ def train_cnn_rnn():  # TRAIN
         json.dump(params, outfile, indent=4, sort_keys=True, ensure_ascii=False)
 
 
+def load_trained_params(trained_dir):
+    params = json.loads(open(trained_dir + 'trained_parameters.json').read())
+    words_index = json.loads(open(trained_dir + 'words_index.json').read())
+    labels = json.loads(open(trained_dir + 'labels.json').read())
+
+    with open(trained_dir + 'embeddings.pickle', 'rb') as input_file:
+        fetched_embedding = pickle.load(input_file)
+    embedding_mat = np.array(fetched_embedding, dtype=np.float32)
+    return params, words_index, labels, embedding_mat
+
+
+def load_test_data(test_file, labels):
+    df = pd.read_csv(test_file)
+    select = ['Descript']
+
+    df = df.dropna(axis=0, how='any', subset=['Descript'])
+    test_examples = df[select[0]].apply(lambda x: clean_str(x).split(' ')).tolist()
+
+    num_labels = len(labels)
+    one_hot = np.zeros((num_labels, num_labels), int)
+    np.fill_diagonal(one_hot, 1)
+    label_dict = dict(zip(labels, one_hot))
+
+    y_ = None
+    if 'Category' in df.columns:
+        select.append('Category')
+        y_ = df[select[1]].apply(lambda x: label_dict[x]).tolist()
+
+    not_select = list(set(df.columns) - set(select))
+    df = df.drop(not_select, axis=1)
+    return test_examples, y_, df
+
+
+def map_word_to_index(examples, words_index):
+    x_ = []
+    for example in examples:
+        temp = []
+        for word in example:
+            if word in words_index:
+                temp.append(words_index[word])
+            else:
+                temp.append(0)
+        x_.append(temp)
+    return x_
+
+
+def predict_unseen_data():
+    # trained_dir = sys.argv[1]
+    # print(trained_dir)
+    trained_dir = PRO_FLD + TRA_FLD
+    print(trained_dir)
+    # sys.exit()
+    if not trained_dir.endswith('/'):
+        trained_dir += '/'
+    # test_file = sys.argv[2]
+    test_file = PRO_FLD + 'data/train.csv.zip'
+
+    params, words_index, labels, embedding_mat = load_trained_params(trained_dir)
+    x_, y_, df = load_test_data(test_file, labels)
+    x_ = pad_sentences(x_, forced_sequence_length=params['sequence_length'])
+    x_ = map_word_to_index(x_, words_index)
+
+    x_test, y_test = np.asarray(x_), None
+    if y_ is not None:
+        y_test = np.asarray(y_)
+
+    timestamp = trained_dir.split('/')[-2].split('_')[-1]
+    predicted_dir = PRO_FLD + 'predicted_results_' + timestamp + '/'
+    if os.path.exists(predicted_dir):
+        shutil.rmtree(predicted_dir)
+    os.makedirs(predicted_dir)
+
+    with tf.Graph().as_default():
+        session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+        sess = tf.Session(config=session_conf)
+        with sess.as_default():
+            cnn_rnn = TextCNNRNN(
+                embedding_mat=embedding_mat,
+                non_static=params['non_static'],
+                hidden_unit=params['hidden_unit'],
+                sequence_length=len(x_test[0]),
+                max_pool_size=params['max_pool_size'],
+                filter_sizes=map(int, params['filter_sizes'].split(",")),
+                num_filters=params['num_filters'],
+                num_classes=len(labels),
+                embedding_size=params['embedding_dim'],
+                l2_reg_lambda=params['l2_reg_lambda'])
+
+            def real_len(batches):
+                return [np.ceil(np.argmin(batch + [0]) * 1.0 / params['max_pool_size']) for batch in batches]
+
+            def predict_step(x_batch):
+                feed_dict = {
+                    cnn_rnn.input_x: x_batch,
+                    cnn_rnn.dropout_keep_prob: 1.0,
+                    cnn_rnn.batch_size: len(x_batch),
+                    cnn_rnn.pad: np.zeros([len(x_batch), 1, params['embedding_dim'], 1]),
+                    cnn_rnn.real_len: real_len(x_batch),
+                }
+                predictions = sess.run([cnn_rnn.predictions], feed_dict)
+                return predictions
+
+            checkpoint_file = trained_dir + 'best_model.ckpt'
+            saver = tf.train.Saver(tf.all_variables())
+            saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+            saver.restore(sess, checkpoint_file)
+            logging.critical('{} has been loaded'.format(checkpoint_file))
+
+            batches = batch_iter(list(x_test), params['batch_size'], 1, shuffle=False)
+
+            predictions, predict_labels = [], []
+            for x_batch in batches:
+                batch_predictions = predict_step(x_batch)[0]
+                for batch_prediction in batch_predictions:
+                    predictions.append(batch_prediction)
+                    predict_labels.append(labels[batch_prediction])
+
+            # Save the predictions back to file
+            df['NEW_PREDICTED'] = predict_labels
+            columns = sorted(df.columns, reverse=True)
+            df.to_csv(predicted_dir + 'predictions_all.csv', index=False, columns=columns, sep='|')
+
+            if y_test is not None:
+                y_test = np.array(np.argmax(y_test, axis=1))
+                accuracy = sum(np.array(predictions) == y_test) / float(len(y_test))
+                logging.critical('The prediction accuracy is: {}'.format(accuracy))
+
+            logging.critical('Prediction is complete, all files have been saved: {}'.format(predicted_dir))
+
+
 if __name__ == '__main__':
     print("hello")
-    train_cnn_rnn()
+    if TRAIN:
+        print("train_cnn_rnn")
+        train_cnn_rnn()
+    else:
+        print("predict_unseen_data")
+        predict_unseen_data()
     print("bye")
