fc264f490ea4f506839aa7ef954ded18524cb9e9
2easy4wizzi <giladeini@gmail.com>
2018-08-03 08:18:24 +0300

    a

--- cr/main.py
+++ cr/main.py
@@ -1,32 +1,31 @@
 import re
+import pickle
+import json
+import sys
 import itertools
 import os
 import time
 import shutil
-import pickle
 import pandas as pd
-import json
 from collections import Counter
 import numpy as np
 import tensorflow as tf
 import logging
-# pip.main(['install', 'builtins '])
-# from builtins import enumerate
 from sklearn.model_selection import train_test_split
-import sys
 from collections import defaultdict
-
-sys.path.insert(0, './cr/')
 from text_cnn_rnn import TextCNNRNN
 
 logging.getLogger().setLevel(logging.INFO)
 
 PRO_FLD = ''
 TRA_FLD = 'trained_results_1533109035/'
+USE_TMP_FOLDER = True
 IS_TRAIN = True
-TRAIN_FILE_PATH = PRO_FLD + 'data/shortdata.csv.zip'
+SHOULD_SAVE = True
+RUN_TEST_AFTER_TRAIN = True and SHOULD_SAVE  # if SHOULD_SAVE is false can't restore and run test
+# TRAIN_FILE_PATH = PRO_FLD + 'data/shortdata.csv.zip'
 TRAIN_FILE_PATH = PRO_FLD + 'data/US-Spain.700.csv.zip'
-PRINT_CLASSES_STATS_EACH_X_STEPS = 400
+PRINT_CLASSES_STATS_EACH_X_STEPS = 1
 
 params = {}
 params['batch_size'] = 128
@@ -38,39 +37,36 @@ params['hidden_unit'] = 300
 params['l2_reg_lambda'] = 0.0
 params['max_pool_size'] = 4
 params['non_static'] = False
-params['num_epochs'] = 1
+params['num_epochs'] = 10
 params['num_filters'] = 32
 
 
 def clean_str(s):  # DATA
-    s = re.sub(r"[^A-Za-z0-9:(),!?\'\`]", " ", s)
-    s = re.sub(r" : ", ":", s)
-    s = re.sub(r"\'s", " \'s", s)
-    s = re.sub(r"\'ve", " \'ve", s)
-    s = re.sub(r"n\'t", " n\'t", s)
-    s = re.sub(r"\'re", " \'re", s)
-    s = re.sub(r"\'d", " \'d", s)
-    s = re.sub(r"\'ll", " \'ll", s)
-    s = re.sub(r",", " , ", s)
-    s = re.sub(r"!", " ! ", s)
-    s = re.sub(r"\(", " \( ", s)
-    s = re.sub(r"\)", " \) ", s)
-    s = re.sub(r"\?", " \? ", s)
-    s = re.sub(r"\s{2,}", " ", s)
+    # s = re.sub(r"[^A-Za-z0-9:(),!?\'`]", " ", s)
+    # s = re.sub(r" : ", ":", s)
+    # s = re.sub(r"\'s", " \'s", s)
+    # s = re.sub(r"\'ve", " \'ve", s)
+    # s = re.sub(r"n\'t", " n\'t", s)
+    # s = re.sub(r"\'re", " \'re", s)
+    # s = re.sub(r"\'d", " \'d", s)
+    # s = re.sub(r"\'ll", " \'ll", s)
+    # s = re.sub(r",", " , ", s)
+    # s = re.sub(r"!", " ! ", s)
+    # s = re.sub(r"\(", " \( ", s)
+    # s = re.sub(r"\)", " \) ", s)
+    # s = re.sub(r"\?", " \? ", s)
+    # s = re.sub(r"\s{2,}", " ", s)
     return s.strip().lower()
 
 
 def load_embeddings(vocabulary):
-    # print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
     word_embeddings = {}
     for word in vocabulary:
         word_embeddings[word] = np.random.uniform(-0.25, 0.25, 300)
-    # print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
     return word_embeddings
 
 
 def pad_sentences(sentences, padding_word="<PAD/>", forced_sequence_length=None):
-    # print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
     """Pad setences during training or prediction"""
     # longest_sen = ""
     # max_found = 0
@@ -99,16 +95,13 @@ def pad_sentences(sentences, padding_word="<PAD/>", forced_sequence_length=None)
         else:
             padded_sentence = sentence + [padding_word] * num_padding
         padded_sentences.append(padded_sentence)
-    # print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
     return padded_sentences
 
 
 def build_vocab(sentences):
-    # print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
     word_counts = Counter(itertools.chain(*sentences))
     vocabulary_inv = [word[0] for word in word_counts.most_common()]
     vocabulary = {word: index for index, word in enumerate(vocabulary_inv)}
-    # print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
     return vocabulary, vocabulary_inv
 
 
@@ -132,7 +125,7 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
 
 
 def load_data(filename):
-    print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
+    print("Entering function load_data")
     df = pd.read_csv(filename, compression='zip')
     selected = ['Category', 'Descript']
     non_selected = list(set(df.columns) - set(selected))
@@ -147,20 +140,20 @@ def load_data(filename):
     np.fill_diagonal(one_hot, 1)
     label_dict = dict(zip(labels, one_hot))
 
-    x_raw = df[selected[1]].apply(lambda x: clean_str(x).split(' ')).tolist()
-    y_raw = df[selected[0]].apply(lambda y: label_dict[y]).tolist()
+    x_raw = df[selected[1]].apply(lambda x_l: clean_str(x_l).split(' ')).tolist()
+    y_raw = df[selected[0]].apply(lambda y_l: label_dict[y_l]).tolist()
 
     x_raw = pad_sentences(x_raw)
     vocabulary, vocabulary_inv = build_vocab(x_raw)
 
     x = np.array([[vocabulary[word] for word in sentence] for sentence in x_raw])
     y = np.array(y_raw)
-    print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
+    print("Leaving function load_data")
     return x, y, vocabulary, vocabulary_inv, df, labels  # DATA END
 
 
 def train_cnn_rnn():  # TRAIN
-    print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
+    print("Entering function train_cnn_rnn")
     x_, y_, vocabulary, vocabulary_inv, df, labels = load_data(TRAIN_FILE_PATH)
     # Assign a 300 dimension vector to each word
     word_embeddings = load_embeddings(vocabulary)
@@ -178,6 +171,8 @@ def train_cnn_rnn():  # TRAIN
 
     # Create a directory, everything related to the training will be saved in this directory
     timestamp = str(int(time.time()))
+    if USE_TMP_FOLDER:
+        timestamp = "temp"
     trained_dir = PRO_FLD + 'trained_results_' + timestamp + '/'
     if os.path.exists(trained_dir):
         shutil.rmtree(trained_dir)
@@ -224,8 +219,8 @@ def train_cnn_rnn():  # TRAIN
                     cnn_rnn.pad: np.zeros([len(x_batch), 1, params['embedding_dim'], 1]),
                     cnn_rnn.real_len: real_len(x_batch),
                 }
-                _, step, loss, accuracy = sess.run([train_op, global_step, cnn_rnn.loss, cnn_rnn.accuracy],
-                                                   feed_dict)
+                _, _, _, _ = sess.run([train_op, global_step, cnn_rnn.loss, cnn_rnn.accuracy], feed_dict)
+                return
 
             def dev_step(x_batch, y_batch):
                 feed_dict = {
@@ -236,19 +231,18 @@ def train_cnn_rnn():  # TRAIN
                     cnn_rnn.pad: np.zeros([len(x_batch), 1, params['embedding_dim'], 1]),
                     cnn_rnn.real_len: real_len(x_batch),
                 }
-                step, loss, accuracy, num_correct, predictions = sess.run(
-                    [global_step, cnn_rnn.loss, cnn_rnn.accuracy, cnn_rnn.num_correct, cnn_rnn.predictions],
-                    feed_dict)
-                return accuracy, loss, num_correct, predictions
+                step, loss_l, accuracy_l, num_correct, predictions_l = sess.run(
+                    [global_step, cnn_rnn.loss, cnn_rnn.accuracy, cnn_rnn.num_correct, cnn_rnn.predictions], feed_dict)
+                return accuracy_l, loss_l, num_correct, predictions_l
 
             def print_stats(stat_dict_total, stat_dict_correct):
                 for key in stat_dict_total:
-                    msg = "     Class {}: ({}/{}) -> accuracy: {}%"
+                    msg = "     Class {}: ({}/{}) -> accuracy: {:.4f}%"
                     temp = 0
                     if key in stat_dict_correct:
                         temp = stat_dict_correct[key]
-                    my_acc = float(temp) / float(stat_dict_total[key])
-                    print(msg.format(key, temp, stat_dict_total[key], my_acc))
+                    my_acc_l = (float(temp) / float(stat_dict_total[key]))*100
+                    print(msg.format(key, temp, stat_dict_total[key], my_acc_l))
                 return
 
             saver = tf.train.Saver()
@@ -259,7 +253,7 @@ def train_cnn_rnn():  # TRAIN
             best_accuracy, best_at_step = 0, 0
             number_of_steps_in_total = len(x_train) / 128 + 1  # steps
             number_of_steps_in_total *= params['num_epochs']
-            logging.info("There will be {} steps total".format(int(number_of_steps_in_total)))
+            print("***There will be {} steps total".format(int(number_of_steps_in_total)))
 
             stat_dict_all_total, stat_dict_all_correct = defaultdict(int), defaultdict(int)
             # Train the model with x_train and y_train
@@ -276,64 +270,81 @@ def train_cnn_rnn():  # TRAIN
                     for dev_batch in dev_batches:
                         x_dev_batch, y_dev_batch = zip(*dev_batch)
                         acc, loss, num_dev_correct, predictions = dev_step(x_dev_batch, y_dev_batch)
-                        ind, count_good = 0, 0
+                        ind = 0
                         for p in predictions:
-                            real_class_value = np.argmax(y_dev_batch[ind])
+                            real_class_value = int(np.argmax(y_dev_batch[ind]))
                             real_class_label = labels[real_class_value]
                             stat_dict_step_total[real_class_label] += 1
                             if p == real_class_value:
-                                count_good += 1
                                 stat_dict_step_correct[real_class_label] += 1
                             ind += 1
                         total_dev_correct += num_dev_correct
                     accuracy = float(total_dev_correct) / len(y_dev)
 
                     # Stats prints
-                    mes = "STEP {} - ({}/{}) -> accuracy: {}%"
+                    mes = "STEP {} - ({}/{}) -> accuracy: {:.4f}%"
                     print(mes.format(current_step, int(total_dev_correct), len(y_dev), accuracy*100))
                     if current_step % PRINT_CLASSES_STATS_EACH_X_STEPS == 0:
                         print_stats(stat_dict_step_total, stat_dict_step_correct)
 
                     if accuracy > best_accuracy:
                         best_accuracy, best_at_step = accuracy, current_step
-                        path = saver.save(sess, checkpoint_prefix, global_step=current_step)
-                        logging.info('    Saved model {} at step {}'.format(path, best_at_step))
-                        logging.info('    Best accuracy {} at step {}'.format(best_accuracy, best_at_step))
+                        if SHOULD_SAVE:
+                            path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                            logging.info('    Saved model {} at step {}'.format(path, best_at_step))
+                        logging.info('    Best accuracy {:.4f}% at step {}'.format(best_accuracy*100, best_at_step))
                 stat_dict_all_total = dict(Counter(stat_dict_all_total)+Counter(stat_dict_step_total))
                 stat_dict_all_correct = dict(Counter(stat_dict_all_correct)+Counter(stat_dict_step_correct))
-            print('Training is complete, testing the best model on x_test and y_test')
+            train_msg = '***Training is complete. Best accuracy {:.4f}% at step {}'
+            print(train_msg.format(best_accuracy*100, best_at_step))
             # Stats prints
             print_stats(stat_dict_all_total, stat_dict_all_correct)
             # Save the model files to trained_dir. predict.py needs trained model files.
-            saver.save(sess, trained_dir + "best_model.ckpt")
+            if SHOULD_SAVE:
+                saver.save(sess, trained_dir + "best_model.ckpt")
 
             # Evaluate x_test and y_test
-            saver.restore(sess, checkpoint_prefix + '-' + str(best_at_step))
-            test_batches = batch_iter(list(zip(x_test, y_test)), params['batch_size'], 1, shuffle=False)
-            total_test_correct = 0
-            for test_batch in test_batches:
-                x_test_batch, y_test_batch = zip(*test_batch)
-                acc, loss, num_test_correct, predictions = dev_step(x_test_batch, y_test_batch)
-                total_test_correct += int(num_test_correct)
-            print('Accuracy on test set: {}'.format(float(total_test_correct) / len(y_test)))
+            if RUN_TEST_AFTER_TRAIN:
+                print('***Testing...')
+                saver.restore(sess, checkpoint_prefix + '-' + str(best_at_step))
+                test_batches = batch_iter(list(zip(x_test, y_test)), params['batch_size'], 1, shuffle=False)
+                total_test_correct = 0
+                test_stat_dict_total, test_dict_correct = defaultdict(int), defaultdict(int)
+                for test_batch in test_batches:
+                    x_test_batch, y_test_batch = zip(*test_batch)
+                    acc, loss, num_test_correct, predictions = dev_step(x_test_batch, y_test_batch)
+                    ind = 0
+                    for p in predictions:
+                        real_class_value = int(np.argmax(y_test_batch[ind]))
+                        real_class_label = labels[real_class_value]
+                        test_stat_dict_total[real_class_label] += 1
+                        if p == real_class_value:
+                            test_dict_correct[real_class_label] += 1
+                        ind += 1
+                    total_test_correct += int(num_test_correct)
+                my_acc = (float(total_test_correct) / float(len(y_test))) * 100
+                acc_msg = 'Accuracy on test set - ({}/{}) -> accuracy: {:.4f}%'
+                print(acc_msg.format(total_test_correct, len(y_test), my_acc))
+                # Stats prints
+                print_stats(test_stat_dict_total, test_dict_correct)
 
-    # Save trained parameters and files since predict.py needs them
-    with open(trained_dir + 'words_index.json', 'w') as outfile:
-        json.dump(vocabulary, outfile, indent=4, ensure_ascii=False)
-    with open(trained_dir + 'embeddings.pickle', 'wb') as outfile:
-        pickle.dump(embedding_mat, outfile)
-    with open(trained_dir + 'labels.json', 'w') as outfile:
-        json.dump(labels, outfile, indent=4, ensure_ascii=False)
-
-    params['sequence_length'] = x_train.shape[1]
-    with open(trained_dir + 'trained_parameters.json', 'w') as outfile:
-        json.dump(params, outfile, indent=4, sort_keys=True, ensure_ascii=False)
-    print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
+    # # Save trained parameters and files since predict.py needs them
+    # with open(trained_dir + 'words_index.json', 'w') as outfile:
+    #     json.dump(vocabulary, outfile, indent=4, ensure_ascii=False)
+    # with open(trained_dir + 'embeddings.pickle', 'wb') as outfile:
+    #     pickle.dump(embedding_mat, outfile)
+    # with open(trained_dir + 'labels.json', 'w') as outfile:
+    #     json.dump(labels, outfile, indent=4, ensure_ascii=False)
+    #
+    # params['sequence_length'] = x_train.shape[1]
+    # with open(trained_dir + 'trained_parameters.json', 'w') as outfile:
+    #     json.dump(params, outfile, indent=4, sort_keys=True, ensure_ascii=False)
+    print("Leaving function train_cnn_rnn")
     return
 
 
 # def load_trained_params(trained_dir):
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
+#     print("Entering function load_trained_params")
 #     params = json.loads(open(trained_dir + 'trained_parameters.json').read())
 #     words_index = json.loads(open(trained_dir + 'words_index.json').read())
 #     labels = json.loads(open(trained_dir + 'labels.json').read())
@@ -341,12 +352,12 @@ def train_cnn_rnn():  # TRAIN
 #     with open(trained_dir + 'embeddings.pickle', 'rb') as input_file:
 #         fetched_embedding = pickle.load(input_file)
 #     embedding_mat = np.array(fetched_embedding, dtype=np.float32)
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
+#     print("Leaving function load_trained_params")
 #     return params, words_index, labels, embedding_mat
 
 
 # def load_test_data(test_file, labels):
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
+#     print("Entering function load_test_data")
 #     df = pd.read_csv(test_file)
 #     select = ['Descript']
 #
@@ -365,12 +376,11 @@ def train_cnn_rnn():  # TRAIN
 #
 #     not_select = list(set(df.columns) - set(select))
 #     df = df.drop(not_select, axis=1)
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
+#     print("Leaving function load_test_data")
 #     return test_examples, y_, df
 
 
 # def map_word_to_index(examples, words_index):
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
 #     x_ = []
 #     for example in examples:
 #         temp = []
@@ -380,12 +390,11 @@ def train_cnn_rnn():  # TRAIN
 #             else:
 #                 temp.append(0)
 #         x_.append(temp)
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
 #     return x_
 
 
 # def predict_unseen_data():
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "start"))
+#     print("Entering function predict_unseen_data")
 #     trained_dir = PRO_FLD + TRA_FLD
 #     print(trained_dir)
 #     if not trained_dir.endswith('/'):
@@ -401,7 +410,7 @@ def train_cnn_rnn():  # TRAIN
 #     if y_ is not None:
 #         y_test = np.asarray(y_)
 #
-#     timestamp = trained_dir.split('/')[-2].split('_')[-1]
+#     timestamp = trained_dir.split('/')[-2].split('_')[-1] # check when using TEMP FOLDER
 #     predicted_dir = PRO_FLD + 'predicted_results_' + timestamp + '/'
 #     if os.path.exists(predicted_dir):
 #         shutil.rmtree(predicted_dir)
@@ -463,16 +472,15 @@ def train_cnn_rnn():  # TRAIN
 #                 print('The prediction accuracy is: {}'.format(accuracy))
 #
 #             print('Prediction is complete, all files have been saved: {}'.format(predicted_dir))
-#     print("function {} {}".format(sys._getframe().f_code.co_name, "exit"))
+#     print("Leaving function predict_unseen_data")
 #     return
 
 
 if __name__ == '__main__':
-    print("function {} {}".format(__name__, "start"))
+    print("Entering function __main__")
     if IS_TRAIN:
         train_cnn_rnn()
     else:
         pass
         # predict_unseen_data()
-
-    print("function {} {}".format(__name__, "exit"))
+    print("Leaving function __main__")
