1a685fb9c98b53bfb8e2692f49e687a2b81b28d6
2easy4wizzi <giladeini@gmail.com>
2018-08-14 19:31:11 +0300

    a

--- bi/src/bi.py
+++ bi/src/bi.py
@@ -22,11 +22,11 @@ logging.getLogger().setLevel(logging.INFO)
 LINE_FROM_CLASS = 5000
 MINIMUM_ROW_LENGTH = 25
 MAXIMUM_ROW_LENGTH = 150
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 LSTM_HIDDEN_UNITS = 64
 EPOCHS = 1
 KEEP_PROB = 0.5
-
+SHOULD_SAVE = True
 # GENERAL VARS
 PRO_FLD = '../'
 DATA_DIR = 'input/'
@@ -34,8 +34,8 @@ EMB_FILE = 'glove.6B.50d.txt'
 EMB_DIM = 50
 EMB_FILE_PATH = PRO_FLD + DATA_DIR + EMB_FILE
 # DATA_FILE = '2way_rus_usa{}-{}.txt'.format(MINIMUM_ROW_LENGTH, MAXIMUM_ROW_LENGTH)
-DATA_FILE = '2way_short{}-{}.txt'.format(MINIMUM_ROW_LENGTH, MAXIMUM_ROW_LENGTH)
-DATA_FILE_PATH = PRO_FLD + DATA_DIR + DATA_FILE
+DATA_FILE = '2way_short{}-{}'.format(MINIMUM_ROW_LENGTH, MAXIMUM_ROW_LENGTH)
+DATA_FILE_PATH = PRO_FLD + DATA_DIR + DATA_FILE + '.txt'
 COUNT_WORD = 20  # if a sentence has COUNT_WORD of the same word - it's a bad sentence (just a troll)
 
 # existing FILES
@@ -87,14 +87,17 @@ def load_data(data_full_path, shuffle=False):
         data_x.append(sentence)
 
     l_unique_labels_list = np.unique(np.array(labels_str))
-    l_unique_labels_dict = {}
+    l_unique_labels_to_ind = {}
+    l_unique_ind_to_labels = {}
     for i in range(len(l_unique_labels_list)):
-        l_unique_labels_dict[l_unique_labels_list[i]] = i
+        l_unique_labels_to_ind[l_unique_labels_list[i]] = i
+        l_unique_ind_to_labels[i] = l_unique_labels_list[i]
 
-    print('Our {} labels dictionary ={}'.format(len(l_unique_labels_dict), l_unique_labels_dict))
+    print('Our {} labels to index dictionary ={}'.format(len(l_unique_labels_to_ind), l_unique_labels_to_ind))
+    print('Our {} index to labels dictionary ={}'.format(len(l_unique_ind_to_labels), l_unique_ind_to_labels))
 
     for i in range(len(labels_str)):
-        labels_int.append(l_unique_labels_dict[labels_str[i]])
+        labels_int.append(l_unique_labels_to_ind[labels_str[i]])
 
     # split data to train - test
     split_train_test_percent = 0.9
@@ -124,7 +127,7 @@ def load_data(data_full_path, shuffle=False):
     print('x_train: {}, x_dev: {}, x_test: {}'.format(len(l_train_x), len(l_dev_x), len(l_test_x)))
     print('y_train: {}, y_dev: {}, y_test: {}'.format(len(l_train_y), len(l_dev_y), len(l_test_y)))
 
-    return l_train_x, l_train_y, l_dev_x, l_dev_y, l_test_x, l_test_y, l_unique_labels_dict
+    return l_train_x, l_train_y, l_dev_x, l_dev_y, l_test_x, l_test_y, l_unique_labels_to_ind, l_unique_ind_to_labels
 
 
 # creates 2 objects
@@ -148,7 +151,7 @@ def load_emb(emb_full_path):
 
 def get_bidirectional_rnn_model(l_emb_mat):
     tf.reset_default_graph()
-    num_classes = len(gl_unique_labels_dict)
+    num_classes = len(gl_label_to_ind)
     input_data_x_batch = tf.placeholder(tf.int32, [BATCH_SIZE, MAXIMUM_ROW_LENGTH])
     input_labels_batch = tf.placeholder(tf.float32, [BATCH_SIZE, num_classes])
     keep_prob_pl = tf.placeholder(tf.float32)
@@ -175,16 +178,17 @@ def get_bidirectional_rnn_model(l_emb_mat):
     prediction = (tf.matmul(outputs_as_value, weight) + bias)
 
     correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(input_labels_batch, 1))
+
     acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     l_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=input_labels_batch))
     l_optimizer = tf.train.AdamOptimizer().minimize(l_loss)
-
-    return input_data_x_batch, input_labels_batch, keep_prob_pl, l_optimizer, l_loss, acc
+    l_num_correct = tf.reduce_sum(tf.cast(correct_pred, tf.float32))
+    return input_data_x_batch, input_labels_batch, keep_prob_pl, l_optimizer, l_loss, acc, l_num_correct, correct_pred
 
 
 def convert_to_array(label_value):
-    label_zero_one_vec = [0] * len(gl_unique_labels_dict)
+    label_zero_one_vec = [0] * len(gl_label_to_ind)
     label_zero_one_vec[label_value - 1] = 1
     return label_zero_one_vec
 
@@ -196,29 +200,108 @@ def get_batch_sequential(data_x, data_y, batch_num, batch_size):
     return batch, labels
 
 
-def train(l_train_x, l_train_y):
+def train_step_func(sess, x_batch, y_batch):
+    feed_dict = {input_data: x_batch,
+                 input_labels: y_batch,
+                 keep_prob: KEEP_PROB
+                 }
+    _, batch_loss_trn, batch_acc_trn = sess.run([optimizer, loss, accuracy], feed_dict)
+    return batch_loss_trn, batch_acc_trn
+
+
+def dev_step_func(sess, x_batch, y_batch):
+    feed_dict = {input_data: x_batch,
+                 input_labels: y_batch,
+                 keep_prob: KEEP_PROB}
+    batch_loss_dev, batch_acc_dev, batch_num_correct, preds = sess.run([loss, accuracy, num_correct, predictions], feed_dict)
+    # loss_l, accuracy_l, num_correct, predictions_l = sess.run(
+    #     [cnn_rnn.loss, cnn_rnn.accuracy, cnn_rnn.num_correct, cnn_rnn.predictions], feed_dict)
+    # return accuracy_l, loss_l, num_correct, predictions_l
+    return batch_loss_dev, batch_acc_dev, batch_num_correct, preds
+
+
+def print_stats(stat_dict_total, stat_dict_correct):
+    longest_key = 0
+    for key in stat_dict_total:
+        if len(key) > longest_key:
+            longest_key = len(key)
+    for key in stat_dict_total:
+        my_msg = "     Class {:{}s}: ({}/{}) -> accuracy: {:.4f}%"
+        temp = 0
+        if key in stat_dict_correct:
+            temp = stat_dict_correct[key]
+        my_acc_l = (float(temp) / float(stat_dict_total[key])) * 100
+        print(my_msg.format(key, longest_key, temp, stat_dict_total[key], my_acc_l))
+    return
+
+
+def train(l_train_x, l_train_y, l_dev_x, l_dev_y):
+    timestamp = str(int(time.time()))
+    if USE_TMP_FOLDER:
+        timestamp = "temp"
+    model_dir = PRO_FLD + 'model_' + timestamp + '/'
+    if os.path.exists(model_dir):
+        shutil.rmtree(model_dir)
+    os.makedirs(model_dir)
+    model_full_path = '{}model.ckpt'.format(model_dir)
+
     session_conf = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
     sess = tf.Session(config=session_conf)
     with sess.as_default():
+        saver = tf.train.Saver()
         sess.run(tf.global_variables_initializer())
+        best_accuracy, best_at_epoch = 0, 0
+        batches_num_train = int(math.ceil(len(l_train_y) / BATCH_SIZE))
+        batches_num_dev = int(math.ceil(len(l_dev_y) / BATCH_SIZE))
         for i in range(EPOCHS):
-            print("\nEpoch: {0}/{1}\n".format((i + 1), EPOCHS))
-            batches_num = int(math.ceil(len(train_x) / BATCH_SIZE))
-            for s in range(batches_num):
-                batch_xs, batch_ys = get_batch_sequential(l_train_x, l_train_y, s, BATCH_SIZE)
-                if len(batch_ys) != BATCH_SIZE:
+            print("Epoch: {0}/{1}".format((i + 1), EPOCHS))
+            for train_step in range(batches_num_train):
+                batch_x_trn, batch_y_trn = get_batch_sequential(l_train_x, l_train_y, train_step, BATCH_SIZE)
+                if len(batch_y_trn) != BATCH_SIZE:
                     continue
+                _, _ = train_step_func(sess, batch_x_trn, batch_y_trn)
+                # #if you like to print the train data performance, replace the above line with the next 3 lines
+                # batch_loss_trn, batch_acc_trn = train_step_func(sess, batch_x_trn, batch_y_trn)
+                # msg = "    TRAIN: STEP {}/{}: batch_acc = {:.4f}% , batch loss = {:.4f}"
+                # print(msg.format(train_step + 1, batches_num_train - 1, batch_acc_trn * 100, batch_loss_trn))
 
-                _, batch_loss, batch_acc = sess.run([optimizer, loss, accuracy],
-                                                    feed_dict={input_data: batch_xs, input_labels: batch_ys,
-                                                               keep_prob: KEEP_PROB})
+                # check on dev data
+                total_correct, total_seen, dev_acc = 0, 0, 0
+                stat_dict_step_total, stat_dict_step_correct = defaultdict(int), defaultdict(int)
+                for dev_step in range(batches_num_dev):
+                    batch_x_dev, batch_y_dev = get_batch_sequential(l_dev_x, l_dev_y, dev_step, BATCH_SIZE)
+                    if len(batch_y_dev) != BATCH_SIZE:
+                        continue
+                    _, _, batch_num_correct_dev, l_predictions = dev_step_func(sess, batch_x_dev, batch_y_dev)
+                    # #if you like to print the dev data performance, replace the above line with the next 3 lines
+                    # batch_loss_dev, batch_acc_dev, batch_num_correct_dev, l_predictions = dev_step_func(sess, batch_x_dev, batch_y_dev)
+                    # msg = "        DEV: STEP {}/{}: batch_acc = {:.4f}% , batch loss = {:.4f}"
+                    # print(msg.format(dev_step + 1, batches_num_dev - 1, batch_acc_dev * 100, batch_loss_dev))
 
-                # original: if s % 10
-                # if s % _STEPS_PRINT == 0:
-                msg = "STEP {}/{}: batch_acc = {:.4f}% , batch loss = {:.4f}"
-                print(msg.format(s, batches_num-1, batch_acc*100, batch_loss))
+                    for p in range(len(l_predictions)):  # calculating acc per class
+                        true_val = int(np.argmax(batch_y_dev[p]))
+                        true_lbl = gl_ind_to_label[true_val]
+                        stat_dict_step_total[true_lbl] += 1
+                        if l_predictions[p]:
+                            stat_dict_step_correct[true_lbl] += 1
 
-                # current_error += 1 - batch_acc
+                    total_correct += int(batch_num_correct_dev)  # sum correct predictions for acc
+                    total_seen += BATCH_SIZE
+
+                dev_acc = total_correct/total_seen
+                msg = "    DEV accuracy on epoch {}/{} = {:.4f}%"
+                print_stats(stat_dict_step_total, stat_dict_step_correct)
+                print(msg.format(i + 1, EPOCHS, dev_acc * 100))
+                if dev_acc > best_accuracy:
+                    best_accuracy, best_at_epoch = dev_acc, i + 1
+                    if SHOULD_SAVE:
+                        save_path = saver.save(sess, model_full_path)
+                        logging.info('    Saved model {} at epoch {}'.format(save_path, best_at_epoch))
+                    msg = '    Best accuracy {:.4f}% at epoch {}/{} ({}/{})'
+                    logging.info(msg.format(best_accuracy * 100, best_at_epoch, EPOCHS, total_correct, total_seen))
+            print("###################################################################################################")
+            sys.exit(0)
+            # current_error += 1 - batch_acc
 
             # print("loss avg for epoch {} is {}".format(epoch, current_loss / bacthes_num))
             # train_error_list.append(current_error / bacthes_num)
@@ -241,11 +324,11 @@ def args_print(stage, duration=0):
 if __name__ == '__main__':
     print("Entering function __main__")
     total_start_time = time.time()
-    global gl_word_to_emb_mat_ind, gl_unique_labels_dict
+    global gl_word_to_emb_mat_ind, gl_label_to_ind, gl_ind_to_label
     gl_word_to_emb_mat_ind, emb_mat = load_emb(EMB_FILE_PATH)
-    train_x, train_y, dev_x, dev_y, test_x, test_y, gl_unique_labels_dict = load_data(DATA_FILE_PATH)
-    input_data, input_labels, keep_prob, optimizer, loss, accuracy = get_bidirectional_rnn_model(emb_mat)
-    train(train_x, train_y)
+    train_x, train_y, dev_x, dev_y, test_x, test_y, gl_label_to_ind, gl_ind_to_label = load_data(DATA_FILE_PATH)
+    input_data, input_labels, keep_prob, optimizer, loss, accuracy, num_correct, predictions = get_bidirectional_rnn_model(emb_mat)
+    train(train_x, train_y, dev_x, dev_y)
     # test()
     dur = time.time() - total_start_time
     args_print('End summary', int(dur))
